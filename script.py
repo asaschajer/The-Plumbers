9d73df465cb78f31c4fee301a6606771a08d5251
Dilyara <dilyaraiskarova64@gmail.com>
2026-01-19 23:19:35 +0600

    Create a 'journalists.json' JSON file with all the journalists (under "ontology/occupation_label" key in original data)

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 import json
-from collections import Counter
 
 letters = 'QWERTYUIOPASDFGHJKLZXCVBNM'
 data_name = []
+journalists = []
 
 for i in letters:
     n = i + '_people.json'
@@ -10,7 +10,16 @@ for i in letters:
 
 # if you want to work with the json file(s) you should do it inside the following for loop
 for n in range(26):
-    with open(f"The-Plumbers/data/{data_name[n]}", encoding='utf-8') as file:
-        data = file.read()
+    with open(f"data/{data_name[n]}", encoding='utf-8') as file:
+        data = json.load(file)
+    for d in data:
+        occupation = d.get('ontology/occupation_label')
+        if occupation == 'Journalist' or ((isinstance(occupation, list)) and 'Journalist' in occupation):
+            journalists.append(d)
+    with open('journalists.json', 'w', encoding='utf-8') as file:
+        json.dump(journalists, file, indent=4, ensure_ascii=0)
 
-print(data) #test if works
+with open('journalists.json', encoding='utf-8') as file:
+    line = json.load(file)
+
+
