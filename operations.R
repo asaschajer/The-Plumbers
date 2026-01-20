acdadacfa4e141cd5ced9a09cd723cf0b3daa791
asaschajer <aurore.schajer@outlook.fr>
2026-01-20 14:01:23 +0100

    Calculate the average life length before transforming the string into int

--- operations.R
+++ operations.R
@@ -1,14 +1,9 @@
-# Calculate the Journalists' life length
-
-library(jsonlite)
+# Calculate the journalist life length
 library(tidyverse)
 
-# Then we have to load JSON data
-d <- fromJSON("journalists.json")
 
-# Then convert  the JSON data to dataframe
 # df["ontology/deathDate" == 'NULL'] <- NA
-df <- as.data.frame(d) |>
+df <- read_csv('journalists.csv') |>
   select("title", "ontology/birthDate", "ontology/birthYear","ontology/occupation_label", "ontology/deathDate", "ontology/deathYear",)|>
   na.omit()|>
   rename('ontology_birthYear' = 'ontology/birthYear', 'ontology_deathYear' = 'ontology/deathYear' )|>
