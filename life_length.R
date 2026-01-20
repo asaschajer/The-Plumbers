dd038869d0e75b42b1b22da50c2386ad9c7cf149
asaschajer <aurore.schajer@outlook.fr>
2026-01-20 16:02:56 +0100

    Create life length bar plot and save the plot as PNG

--- life_length.R
+++ life_length.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(readr)
 
-df <- read_csv('journalists.csv') |>
+life_length_plot <- read_csv('journalists.csv') |>
   select("title", "ontology/occupation_label", "ontology/birthYear", "ontology/deathYear")|>
   na.omit()|>
   rename('ontology_birthYear' = 'ontology/birthYear', 'ontology_deathYear' = 'ontology/deathYear' )|>
@@ -10,5 +10,8 @@ df <- read_csv('journalists.csv') |>
   mutate(life_length = ontology_deathYear - ontology_birthYear)|>
   ggplot()+
   aes(x = life_length)+
-  labs(x = "Life lengths (Yrs)", y = 'Number of journalists', title = 'Differences in life lengths among journalists')+
+  labs(x = "Life lengths (Yrs)", y = 'Number of journalists', title = 'Differences in life lengths among journalists', subtitle = 'DQ: Are there any distinct differences in the life lengths of different journalists?' )+
+  theme(plot.title = element_text(face = "bold", size = 14))+
   geom_bar(position = 'dodge')
+
+ggsave('life_length.png', plot = life_length_plot)
