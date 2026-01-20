5c3162481a73a0e34d716c82bca31cba52d2af02
¨pablo¨ <pabloricosanchez19@gmail.com>
2026-01-20 15:28:55 +0100

    places and years to see if war.

--- Place.R
+++ Place.R
@@ -1,12 +1,39 @@
-library(jsonlite)
+
 library(tidyverse)
 
-# Then we have to load JSON data
-d <- fromJSON("journalists.json")
+# 1. read the csv
+journalists_raw <- read_csv("journalists.csv", show_col_types = FALSE)
 
-# Then convert  the JSON data to dataframe
-df <- as.data.frame(d) |>
-  select('ontology/deathPlace_label')
+#Keep only the colums we are interested in. Death year and death place 
+journalists_small <- journalists_raw |>
+  select(`ontology/deathYear`, `ontology/deathPlace_label`)
 
-# At last print the output dataf
-print(df)
+# Top places of death (mas frecuentes)
+# This shit is to get rid  NA :  x == x  es FALSE/NA onlyo cuando x es NA
+top_places <- journalists_small |>
+  filter(`ontology/deathPlace_label` == `ontology/deathPlace_label`) |>
+  filter(`ontology/deathPlace_label` != "") |>
+  group_by(`ontology/deathPlace_label`) |>
+  summarise(n = n()) |>
+  arrange(desc(n))
+
+top_places
+
+#  Top years
+top_years <- journalists_small |>
+  filter(`ontology/deathYear` == `ontology/deathYear`) |>
+  group_by(`ontology/deathYear`) |>
+  summarise(n = n()) |>
+  arrange(desc(n))
+
+top_years
+
+
+
+
+
+
+
+
+
+
