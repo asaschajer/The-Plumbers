79ad939df63bae1ea21af4aec7719fd5fd61397c
¨pablo¨ <pabloricosanchez19@gmail.com>
2026-01-20 14:09:48 +0100

    commiting before I can pull

--- Place.R
+++ Place.R
@@ -1,4 +1,12 @@
-#place of death 
+library(jsonlite)
 library(tidyverse)
-data_frame<-r
-glimpse(data_frame)
+
+# Then we have to load JSON data
+d <- fromJSON("journalists.json")
+
+# Then convert  the JSON data to dataframe
+df <- as.data.frame(d) |>
+  select('ontology/deathPlace_label')
+
+# At last print the output dataf
+print(df)
--- library(jsonlite).R
+++ library(jsonlite).R
@@ -1,4 +1,3 @@
-
 library(jsonlite)
 library(tidyverse)
 
