6119daf2a4753cec798d67ea903b4eddb29d9150
Eleonora Ricceri <e.ricceri@students.uu.nl>
2026-01-20 14:21:07 +0100

    gg

--- df_journalists.R
+++ df_journalists.R
@@ -5,8 +5,12 @@ library(tidyverse)
 d <- fromJSON("journalists.json")
 
 # Then convert  the JSON data to dataframe
-df <- as.data.frame(d) |>
-  select('ontology/deathPlace_label')
+df <- as.data.frame(d)|>
+  select('ontology/deathCause_label') |>
+  na.omit()
+
 
 # At last print the output dataf
-print(df)
+#print(df)
+glimpse(d)
+
--- operations Nora.R
+++ operations Nora.R
@@ -1,7 +1,20 @@
 library(tidyverse)
-data_frame<-read_csv('.csv')
+library(jsonlite)
+
+# Then we have to load JSON data
+d <- fromJSON("journalists.json")
+
+# Then convert  the JSON data to dataframe
+df <- as.data.frame(d) |>
+  select('ontology/deathCause_label') |>
+  
+
+# At last print the output dataf
+print(df)
+
 #Exploring data set
-glimpse(data_frame)
-# distinct(data_frame)
-# head(data_frame)
+glimpse(df)
+#distinct(data_frame)
+#head(data_frame)
+
 
