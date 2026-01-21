77bfefb1ecc2fac46afd72a694601de695c3d159
asaschajer <aurore.schajer@outlook.fr>
2026-01-21 11:55:49 +0100

    Final plot for journalists' deaths by year with correct formatting

--- placedeathandyesrdeath.R
+++ placedeathandyesrdeath.R
@@ -13,15 +13,18 @@ deaths_by_year <- journalists_raw |>
   group_by(`ontology/deathYear`) |>
   summarise(n = n()) |>
   filter(`ontology/deathYear`>1900 & nchar(`ontology/deathYear`) == 4)|>
-  arrange(`ontology/deathYear`)
+  arrange(`ontology/deathYear`)|>
+  mutate(`ontology/deathYear` = as.numeric(`ontology/deathYear`))
 
-# 4) Bar plot (column plot) of the count by year
-ggplot(data = deaths_by_year) +
+# 4) Line plot of the count by year
+count_per_year_plot <- ggplot(data = deaths_by_year) +
   aes(x = `ontology/deathYear`, y = n, group = 1)+
-  labs(x = "Death year", y = "Number of deaths ", title = "Journalist deaths by year ")+
+  labs(x = "Death year", y = "Number of deaths ", title = "Journalists' deaths by year ", subtitle = "DQ: Are there any differences in the number of journalists' deaths \nrecorded between years?")+
   theme(plot.title = element_text(face = "bold", size = 14))+
+  scale_x_continuous(limits = c(1900, 2020), breaks = c(1900, 1920, 1940, 1960, 1980, 2000, 2020))+
   geom_line(color="#697fb3ff", size=1)
 
+ggsave('count_per_year_plot.png', plot = count_per_year_plot)
 
 # ----------------------------
 # PLOT 2: Top 10 lugares
