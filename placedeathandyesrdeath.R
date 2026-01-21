e111edaefc5869114f61a9a1cb05ac918b6054d1
asaschajer <aurore.schajer@outlook.fr>
2026-01-21 11:29:44 +0100

    changing/formatting pablo's graphs and script on Deaths/year

--- placedeathandyesrdeath.R
+++ placedeathandyesrdeath.R
@@ -1,38 +1,34 @@
 library(tidyverse)
+library(ggplot2)
 
-# 1) Leer el CSV
-journalists_raw <- read_csv("journalists.csv", show_col_types = FALSE)
-
-# 2) Quedarnos con las columnas que necesitamos
-journalists_small <- journalists_raw |>
+# 1) Read CSV file
+journalists_raw <- read_csv("journalists.csv", show_col_types = FALSE)|>
   select(`ontology/deathYear`, `ontology/deathPlace_label`)
 
-# PLOT 1: Muertes por anio
+# PLOT 1: Deaths per year
 
-
-# 3) Hacer una tabla resumen: conteo por anio
-deaths_by_year <- journalists_small |>
+# 3) Make a summary table: count by year
+deaths_by_year <- journalists_raw |>
   filter(`ontology/deathYear` == `ontology/deathYear`) |>
   group_by(`ontology/deathYear`) |>
   summarise(n = n()) |>
+  filter(`ontology/deathYear`>1900 & nchar(`ontology/deathYear`) == 4)|>
   arrange(`ontology/deathYear`)
 
-# 4) Bar plot (column plot) del conteo por anio
+# 4) Bar plot (column plot) of the count by year
 ggplot(data = deaths_by_year) +
-  aes(x = `ontology/deathYear`, y = n) +
-  geom_col() +
-  labs(
-    x = "Death year",
-    y = "Number of deaths ",
-    title = "Journalist deaths by year "
-  )
+  aes(x = `ontology/deathYear`, y = n, group = 1)+
+  labs(x = "Death year", y = "Number of deaths ", title = "Journalist deaths by year ")+
+  theme(plot.title = element_text(face = "bold", size = 14))+
+  geom_line(color="#697fb3ff", size=1)
+
 
 # ----------------------------
 # PLOT 2: Top 10 lugares
 # ----------------------------
 
 # 5) Hacer una tabla resumen: conteo por lugar
-deaths_by_place <- journalists_small |>
+deaths_by_place <- journalists_raw |>
   filter(`ontology/deathPlace_label` == `ontology/deathPlace_label`) |>
   filter(`ontology/deathPlace_label` != "") |>
   group_by(`ontology/deathPlace_label`) |>
