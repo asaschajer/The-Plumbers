983968f00119324496a9106e7f8c6be9982a9d34
Dilyara <dilyaraiskarova64@gmail.com>
2026-01-22 21:02:49 +0600

    Correct life span line plot aligned with major geopolitical events Save 'his_eras_avr_life_span.png' Ignore lifespan

--- cause+place_bar.R
+++ cause+place_bar.R
@@ -6,22 +6,46 @@ violent_causes <- c(
   "Concussion", 'Concussion | Suicide', "Decapitation", "Shot", "Killed", 'Ballistic trauma'
 )
 
-journalists_death_place_cause <- read_csv("journalists.csv") |>
-  select(
-    "ontology/birthPlace_label",
-    "ontology/deathPlace_label",
-    "ontology/deathCause_label",
-    "ontology/deathYear"
-  ) |>
+journalists <- read_csv('journalists.csv') |>
   rename(
-    birth_place = "ontology/birthPlace_label",
-    death_place = "ontology/deathPlace_label",
-    death_cause = "ontology/deathCause_label",
-    death_year = "ontology/deathYear"
+    birth_year = `ontology/birthYear`, 
+    death_year = `ontology/deathYear`, 
+    birth_place = `ontology/birthPlace_label`, 
+    death_place = `ontology/deathPlace_label`,
+    death_cause = "ontology/deathCause_label"
+  ) |>
+  select( 
+    birth_year, 
+    death_year, 
+    birth_place, 
+    death_place,
+    death_cause
+  )
+
+all_journalists <- journalists |>
+  na.omit()|>
+  left_join(read_csv("all_country_mapping.csv"), by ='death_place')|>
+  select( 
+    birth_country, 
+    death_country, 
+    death_year, 
+    birth_year,
+    death_cause
   ) |>
   mutate(
+    birth_year = as.numeric(birth_year), 
+    death_year = as.numeric(death_year), 
+  ) |>  
+  mutate(
+    bd_coincide = (birth_country == death_country),
+    life_length = death_year - birth_year
+  ) |>
+  filter(death_year >= 1900)
+
+journalists_death_place_cause <- all_journalists |>
+  mutate(
     death_year = as.numeric(death_year),
-    bd_coincide = ifelse(birth_place == death_place,
+    bd_coincide = ifelse(birth_country == death_country,
                          "Same place",
                          "Different place"),
     cause_type = ifelse(death_cause %in% violent_causes, "Violent", "Non-violent")
@@ -33,12 +57,28 @@ plot_data <- journalists_death_place_cause |>
   group_by(bd_coincide, cause_type) |>
   summarise(count = n(), .groups = "drop")
 
-clean_plot_data <- plot_data 
+# plot_data_total <- journalists_death_place_cause |>
+#   group_by(bd_coincide) |>
+#   # summarise(count = n(), .groups = "drop") |>
+#   summarise(total = sum(count, na.rm=TRUE))
 
-# Stacked bar plot
-ggplot( clean_plot_data) +
-  aes(x = bd_coincide, y = count, fill = cause_type) +
-  geom_col(position = 'stack') +
+bd_place_data <- all_journalists |>
+  mutate(
+    bd_coincide = as.integer(birthPlace == deathPlace),
+  bd_differ = as.integer(birthPlace != deathPlace)) 
+
+bd_total <- bd_place_data |>
+  pivot_longer(
+    cols = c(bd_coincide, bd_diffe),
+    names_to = "birth_death",
+    values_to = "coincide_or_differ"
+  ) |>
+  group_by(birth_death) |>
+  summarise(total = sum(coincide_or_differ, na.rm=TRUE))
+
+ggplot(bd_total) +
+  aes(x = birth_death, y = total, fill = cause_type) +
+  geom_col(position = 'dodge') +
   scale_fill_manual(
           values = c(
       "Non-violent" = "#697fb3ff",
@@ -52,42 +92,53 @@ ggplot( clean_plot_data) +
     fill = "Cause of death"
   ) +
   theme_minimal() +
-  theme(plot.title = element_text(face = 'bold', size = 14), axis.text.x = element_text(size = 9), axis.text.y = element_text(size = 9), axis.title = element_text(size = 14))
+  theme(plot.title = element_text(face = 'bold', size = 14), axis.text.x = element_text(size = 9), axis.text.y = element_text(size = 9), axis.title = element_text(size = 14)) 
+  
+
+
+# Stacked bar plot
+ggplot(plot_data) +
+  aes(x = bd_coincide, y = count / sum(count), fill = cause_type) +
+  geom_col(position = 'dodge') +
+  scale_fill_manual(
+          values = c(
+      "Non-violent" = "#697fb3ff",
+      "Violent" = "#c44e52"
+    )) +
+  labs(
+    title = "Journalists' place of death and type of death cause",
+    subtitle = "DQ: How often do journalists die outside their country or place of birth,\nand what might this indicate about professional risk and mobility?",
+    x = "Birth place and death place",
+    y = "Number of journalists",
+    fill = "Cause of death"
+  ) +
+  theme_minimal() +
+  theme(plot.title = element_text(face = 'bold', size = 14), axis.text.x = element_text(size = 9), axis.text.y = element_text(size = 9), axis.title = element_text(size = 14)) 
+  
+
 #Also try:
  #aes(x = bd_coincide, fill = cause_type) +
   #geom_bar(position = "fill") +
   #facet_wrap(~ decade) +
   #scale_y_continuous(labels = scales::percent) 
 
-ggsave("Death_place_vs_cause_type_stacked_bar.png")
+#ggsave("Death_place_vs_cause_type_stacked_bar.png")
 
 # box plot: lifespan by place of death (mobility risk)
 
-lifespan_place_plot <- read_csv("journalists.csv") |>
-  select(
-    "ontology/birthYear",
-    "ontology/deathYear",
-    "ontology/birthPlace_label",
-    "ontology/deathPlace_label"
-  ) |>
-  na.omit() |>
-  rename(
-    birth_year = "ontology/birthYear",
-    death_year = "ontology/deathYear",
-    birth_place = "ontology/birthPlace_label",
-    death_place = "ontology/deathPlace_label"
-  ) |>
+journalists_bd <- all_journalists |>
   mutate(
     birth_year = as.numeric(birth_year),
     death_year = as.numeric(death_year),
     life_length = death_year - birth_year,
-    bd_coincide = ifelse(birth_place == death_place,
+    bd_coincide = ifelse(birth_country == death_country,
                          "Died at birthplace",
                          "Died elsewhere")
   ) |>
-  filter(life_length > 0, death_year >= 1900) |>
-  ggplot() +
-  aes(x = bd_coincide, y = life_length, fill = bd_coincide) +
+  filter(life_length > 0, death_year >= 1900)
+
+  ggplot(journalists_bd) +
+  aes(y = life_length, fill = bd_coincide) +
   geom_boxplot(alpha = 0.7, width = 0.6, fill = "#697fb3ff") +
   labs(
     x = "",
@@ -99,5 +150,5 @@ lifespan_place_plot <- read_csv("journalists.csv") |>
 theme(plot.title = element_text(face = 'bold', size = 14), axis.text.x = element_text(size = 9), axis.text.y = element_text(size = 9), axis.title = element_text(size = 14),
     legend.position = "none")
 
-ggsave("Journalists_lifespan_by_place_of_death.png", plot = lifespan_place_plot)
+#ggsave("Journalists_lifespan_by_place_of_death.png", plot = lifespan_place_plot)
 
--- life_span_his_eras.R
+++ life_span_his_eras.R
@@ -1,4 +1,4 @@
-# Calculate the journalist life length
+# Calculate the journalist life length aligned with historical eras
 library(tidyverse)
 library(dplyr)
 
@@ -29,12 +29,6 @@ journalists <- read_csv('journalists.csv') |>
   filter(death_year >= 1900)|>
   print(n= 705)
 
-journalists_country <- read_csv('journalists.csv') |>
-  select('ontology/deathPlace_label', "ontology/birthYear")|>
-  na.omit()
-
-write.csv(journalists_country, file = "Journalists_country.csv", row.names = FALSE)
-
 life_length_plot <- read_csv('journalists.csv') |>
   select("title", "ontology/occupation_label", "ontology/birthYear", "ontology/deathYear")|>
   na.omit()|>
@@ -43,16 +37,8 @@ life_length_plot <- read_csv('journalists.csv') |>
   mutate(life_length = ontology_deathYear - ontology_birthYear)|>
   filter(life_length > 0)
 
-#   ggplot()+
-#   aes(x = life_length)+
-#   labs(x = "Life lengths (Yrs)", y = 'Number of journalists', title = 'Differences in life lengths among journalists', subtitle = 'DQ: Are there any distinct differences in the life lengths of different journalists?' )+
-#   theme(plot.title = element_text(face = "bold", size = 14))+
-#   geom_bar(position = 'dodge', fill = "#697fb3ff")
 
-# ggsave('life_length.png', plot = life_length_plot, width = 10, height = 5)
-
-#avr life span of journalist per year
-average_life_span <- read_csv('journalists.csv') |>
+read_csv('journalists.csv') |>
   select("title", "ontology/occupation_label", "ontology/birthYear", "ontology/deathYear")|>
   rename('ontology_birthYear' = 'ontology/birthYear', 'ontology_deathYear' = 'ontology/deathYear' )|>
   mutate(ontology_birthYear = as.numeric(ontology_birthYear), ontology_deathYear = as.numeric(ontology_deathYear))|>
@@ -89,86 +75,4 @@ ggplot(average_life_span) +
   )
 
 ggsave('his_eras_avr_life_span.png', width = 10, height = 5)
-
-# year_cat <- function(death_year) {
-#   if (death_year <= 1910){
-#     return("1900-1910")
-#   } else if (death_year <= 1920) {
-#     return("1910-1920")
-#   } else if (death_year <= 1930) {
-#     return("1920-1930")
-#   } else if (death_year <= 1940) {
-#     return("1930-1940")
-#   } else if (death_year <= 1950) {
-#     return("1940-1950")
-#   } else if (death_year <= 1960) {
-#     return("1950-1960")
-#   } else if (death_year <= 1970) {
-#     return("1960-1970")
-#   } else if (death_year <= 1980) {
-#     return("1970-1980")
-#   } else if (death_year <= 1990) {
-#     return("1980-1990")
-#   } else if (death_year <= 2000) {
-#     return("1990-2000")
-#   } else if (death_year <= 2010) {
-#     return("2000-2010")
-#   } else if (death_year <= 2020) {
-#     return("2010-2020")
-#   } else {
-#     return("no info")
-#   }
-# }
-
-# year_grouped <- journalists |>
-#   mutate(year = sapply(death_year, year_cat))
-
-# #Avr life span of journalist dying abroad annd dying in home country throughout the years
-# bar_plot <- year_grouped |>
-#   select('bd_coincide', 'life_length', 'year') |> 
-#   na.omit() |>
-#   group_by(bd_coincide, year) |>
-#   summarise(avr_life_span = mean(life_length), total = n()) |>
-#   print()
-#   # ggplot()+
-#   # aes(x= year, y = avr_life_span, fill = bd_coincide)+
-#   # labs(x = "Average life span (Yrs)", y = "Years", title = "Journalists' average life span based on death place")+
-#   # labs(fill = "Location of death")+
-#   # scale_fill_manual(values = c("#e30000ff", "#80c7f6ff"))+
-#   # scale_fill_discrete(labels = c("Died abroad", "Died in home country"))+
-#   # geom_col(position = "dodge")
-
-
-# # active_years <- read_csv('journalists.csv') |>
-# #   select("title", "ontology/deathYear", 'ontology/activeYears', 'ontology/activeYearsEndDate', 'ontology/activeYearsStartDate', 'ontology/activeYearsEndYear', 'ontology/activeYearsStartYear') |>
-# #   rename(deathYear = "ontology/deathYear", 'endYear' = 'ontology/activeYearsEndYear')
-
-# # death_on_duty_data <- active_years |>
-# #     mutate(
-# #     died_on_duty = as.integer(endYear == deathYear),
-# #   lived = as.integer(endYear != deathYear)) |>
-# #   pivot_longer(
-# #     cols = c(died_on_duty, lived),
-# #     names_to = "death_on_duty",
-# #     values_to = "on_duty_or_not"
-# #   ) |>
-# #   group_by(death_on_duty) |>
-# #   summarise(total = sum(on_duty_or_not, na.rm=TRUE))
-
-
-# # ggplot(death_on_duty_data) +
-# #   aes(x = death_on_duty, y = total / sum(total)) +
-# #   scale_y_continuous(labels = scales::label_percent()) +
-# #     labs (
-# #     title = 'Journalists deceased on duty',
-# #     subtitle = 'DQ: How often do journalists die while still being officially employed?',
-# #     y = 'Number of journalists', # whose birth and death locations differ/coincide
-# #     x = ''
-# #   ) +
-# #   scale_x_discrete(labels = c('Died on duty', 'Died after retirement')) +
-# #   theme_minimal() +
-# #   theme(plot.title = element_text(face = 'bold', size = 14), axis.text.x = element_text(size = 9), axis.text.y = element_text(size = 9), axis.title = element_text(size = 14))+
-# #   geom_col(position = 'dodge', fill = "#697fb3ff") 
-
-# # ggsave('died_on_duty.png')
 
