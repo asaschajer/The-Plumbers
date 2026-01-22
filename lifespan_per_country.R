951978aeafc7dec63c8eb44fd0148e65dec3410f
Eleonora Ricceri <e.ricceri@students.uu.nl>
2026-01-22 10:17:37 +0100

    New commit, dataframe for the averagelifespan for country

--- lifespan_per_country.R
+++ lifespan_per_country.R
@@ -22,20 +22,18 @@ young_journalists <- journalists |>
   filter(life_length <= 50) |>
   na.omit() |>
   left_join(read_csv("region_country_mapping.csv"), by='death_place') |>
-  select(-death_place)
+  select(country, life_length, -death_place)|>
+  group_by(country)|>
+  summarise(mean(life_length))
+  print(young_journalists)
 
 
-
-
-
-avr_life_span_plot <- read_csv('journalists.csv') |>
-  mutate(life_length = ontology_deathYear - ontology_birthYear)|>
-  filter(life_length > 0 & ontology_deathYear > 1900)|>
-  select(`ontology/deathPlace_label`, life_length)|>
-  # mutate(type_lifespan= ifelse(life_length <= 50,"Low_lifespan","High_lifespan"))|>
-  filter(life_length <= 50)
-
-
+# avr_life_span_plot <- read_csv('journalists.csv') |>
+#   mutate(life_length = ontology_deathYear - ontology_birthYear)|>
+#   filter(life_length > 0 & ontology_deathYear > 1900)|>
+#   select(`ontology/deathPlace_label`, life_length)|>
+#   # mutate(type_lifespan= ifelse(life_length <= 50,"Low_lifespan","High_lifespan"))|>
+#   filter(life_length <= 50)
 
   # ggplot(data=avr_life_span_plot)+
   # aes(x = `ontology/deathPlace_label`, y = life_length, fill=type_lifespan)+
